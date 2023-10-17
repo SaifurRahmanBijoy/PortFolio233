b0fbaf0b2f918109a5f9953460595077affbbaaa
Saifur Raman <saifurraman@Saifurs-MacBook-Air.local>
2023-10-17 20:08:53 +0600

    updated colors

--- src/pages/PrimaryButton.js
+++ src/pages/PrimaryButton.js
@@ -4,9 +4,6 @@ import { Link } from "react-scroll";
 const PrimaryButton = ({ children }) => {
   return (
     <div>
-      {/* <button className="bg-transparent btn rounded px-5 py-1 border-slate-600 border text-slate-200 hover:text-white hover:shadow-[inset_20rem_0_0_0] hover:shadow-green-400 duration-[900ms,700ms] transition-[color,box-shadow]">
-        {children}
-      </button> */}
       <Link
         to="Projects"
         smooth
